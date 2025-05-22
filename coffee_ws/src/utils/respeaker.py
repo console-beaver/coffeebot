4b273397ca0c449a01aedd435b45764e0e64b34a
LuisArizmendi13 <larizmen@stanford.edu>
2025-05-22 04:09:15 -0700

    lessgo2

--- coffee_ws/src/utils/respeaker.py
+++ coffee_ws/src/utils/respeaker.py
@@ -1,5 +1,6 @@
 import subprocess
 from speech_recognition_msgs.msg import SpeechRecognitionCandidates
+import os
 
 INTRO_SOUND = '../sounds/ask_order.wav'
 CONFIRM_SOUND_1 = '../sounds/order1.wav'
@@ -9,9 +10,14 @@ CONFIRM_SOUND_3 = '../sounds/order3.wav'
 class ReSpeaker:
     def __init__(self, node):
         self.node = node
-        self.voice_command = None
-        self.intro_path = INTRO_SOUND
-        self.confirm_paths = [CONFIRM_SOUND_1, CONFIRM_SOUND_2, CONFIRM_SOUND_3]
+        self.voice_command = None 
+
+        base_dir = os.path.dirname(os.path.abspath(__file__)) 
+
+        self.intro_path = os.path.join(base_dir, INTRO_SOUND)
+        self.confirm_paths = [os.path.join(base_dir, CONFIRM_SOUND_1), 
+                              os.path.join(base_dir, CONFIRM_SOUND_2), 
+                              os.path.join(base_dir,CONFIRM_SOUND_3)]
 
         self.node.create_subscription(
             SpeechRecognitionCandidates,
